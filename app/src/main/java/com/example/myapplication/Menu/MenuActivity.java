59a910e2eaa9c757dec6cd73019c22f1eb7c1ba2
SylvainRoehrig2 <sylvain.roehrig@gmail.com>
2024-03-22 08:25:04 +0100

    Separate Menu and Game

---
+++ app/src/main/java/com/example/myapplication/Menu/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.Menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.myapplication.GameView;
+
+public class MenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new MenuView(this));
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Menu/MenuView.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.Menu;
+
+import android.content.Context;
+import android.view.View;
+
+import com.example.myapplication.GameThread;
+
+public class MenuView extends View {
+    public MenuView(Context context) {
+        super(context);
+        setFocusable(true);
+    }
+}
